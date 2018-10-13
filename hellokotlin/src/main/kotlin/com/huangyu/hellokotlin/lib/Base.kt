7ec3f812e21bf5f5cf8f3f8afb505e3c32d80183
wangy <szwangyu@yeah.net>
2018-10-13 23:32:01 +0800

    add TODO to own serializer and remove uesless files.

--- hellokotlin/src/main/kotlin/com/huangyu/hellokotlin/lib/Base.kt
+++
@@ -1,5 +0,0 @@
-package com.huangyu.hellokotlin.lib
-
-open class Base {
-    open fun foo(i: Int = 10): Int = i + 100
-}
--- hellokotlin/src/main/kotlin/com/huangyu/hellokotlin/lib/Water.kt
+++
@@ -1,6 +0,0 @@
-package com.huangyu.hellokotlin.lib
-
-class Water : Base() {
-
-    override fun foo(i: Int): Int = i + 1000
-}
