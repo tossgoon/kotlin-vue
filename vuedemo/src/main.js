398f6c4a22623f9274e7c3d1b31405f0598242d7
wangy <szwangyu@yeah.net>
2018-10-18 14:06:32 +0800

    add bootstrap support

--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
-/* eslint-disable no-new */
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 new Vue({
   el: '#app',
   router,
