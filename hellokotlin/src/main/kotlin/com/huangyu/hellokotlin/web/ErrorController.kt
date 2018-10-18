ebad1c606e37cd83d89e5dc10778d01fd5115e7e
wangy <szwangyu@yeah.net>
2018-10-18 10:30:53 +0800

    add global error process controller

---
+++ hellokotlin/src/main/kotlin/com/huangyu/hellokotlin/web/ErrorController.kt
@@ -0,0 +1,17 @@
+package com.huangyu.hellokotlin.web
+
+import com.huangyu.common.web.ApiResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+
+@ControllerAdvice @RestController
+class ErrorController {
+
+    // TODO 目前统一处理所有异常了
+    @ExceptionHandler(value = Exception::class)
+    fun handleException(ex: Exception): ApiResponse {
+        println()
+        return ApiResponse.fail(ex.message)
+    }
+}
