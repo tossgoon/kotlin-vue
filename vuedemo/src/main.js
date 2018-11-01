fe79a2243ca1af4b1c36aa51463599ab59b07b96
wangy <szwangyu@yeah.net>
2018-11-01 18:39:41 +0800

    no message

--- vuedemo/src/main.js
+++ vuedemo/src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
 
 import './directives/index.js' // directives register
 import './plugins/plugins.js' // plugins register
@@ -11,22 +10,6 @@ import './plugins/plugins.js' // plugins register
 import Api from './plugins/api/api.js' // customize route
 import axios from 'axios'
 
-export default ({ app }, inject) => {
-  app.$api = Api
-  app.$axios = axios
-}
-
-=======
-import axios from 'axios'
-import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-
-import './directives/index.js'
-import './plugins/plugins.js'
-
-import Api from './plugins/api/api.js' // customize route
-
->>>>>>> aa33488623aad5f6435721d80ee4082aae8c968b
 let main = {
   install(Vue) {
     Vue.prototype.$api = Api
@@ -35,13 +18,10 @@ let main = {
 }
 Vue.use(main)
 
-<<<<<<< HEAD
-=======
 export default ({ app }, inject) => {
   app.$api = Api
 }
 
->>>>>>> aa33488623aad5f6435721d80ee4082aae8c968b
 new Vue({
   el: '#app',
   router,
--- vuedemo/src/plugins/plugins.js
+++ vuedemo/src/plugins/plugins.js
@@ -14,9 +14,6 @@ import * as plugins from '../plugins/plugins.js'
 for (let plugin in plugins) {
   Vue.use(plugins[plugin])
 }
-<<<<<<< HEAD
 
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
-=======
->>>>>>> aa33488623aad5f6435721d80ee4082aae8c968b
